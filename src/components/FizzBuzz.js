5bc2d8cbf344cba1adcb72371714b468bf61812a
Caroline <carolinegrabowski@Carolines-MacBook-Pro.local>
2019-12-03 16:30:53 -0500

    fixing git here

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -4,8 +4,6 @@ function FizzBuzz() {
     const [myNumber, setMyNumber] = useState(0);
     const [output, setOutput] = useState([]);
 
-    // const iterable = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
     const fizzBuzzTest = (num) => {
         if (num === 0) {
             return ( <p>{ num }</p> );
